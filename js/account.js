32a555a7bc83d8aa7eb28c2558fb7cf1c1da6947
CitizenOneEt <87533861+CitizenOneEt@users.noreply.github.com>
2021-11-03 09:53:52 -0500

    final

--- js/account.js
+++ js/account.js
@@ -18,3 +18,12 @@ function createAccount(){
     });
     document.getElementById("accounts").value = txt;
 }
+timer = null; // stores ID of interval timer
+function delayMsg2() {
+    if (timer === null) {
+        timer = setInterval((function(){document.getElementById("output").innerHTML += "Rudy!"}), 2000);
+    }else {
+        clearInterval(timer);
+        timer = null;
+    }
+}
