11a5d28a12989b624c3c45b2fdea2a7bca6a8fc6
CitizenOneEt <87533861+CitizenOneEt@users.noreply.github.com>
2021-11-08 07:32:44 -0600

    Lab 8

---
+++ js/maze.js
@@ -0,0 +1,40 @@
+$( document ).ready(function() {
+    handler();
+});
+function updateHeader(){
+    setTimeout(changeOnHeader, 1000)
+}
+function changeOnHeader(){
+    $('h2').text('Click the "S" to play')
+}
+function unbind(){
+    $('#start').off("click");
+    $('.boundary, body').off('mouseenter');
+    $('.boundary').css('background-color', 'rgb(231, 231, 231)');
+    updateHeader();
+}
+function handler(){
+    $('#start').on('click',function(){
+        
+        $('.boundary').on('mouseenter', function(){
+            $(this).css('background-color', 'rgb(229, 151, 151)');
+            $('h2').text('You lose!')
+            alert('You Lose')
+            unbind();
+            handler();
+        })
+        $('body').on('mouseenter', function(){
+            $('.boundary').css('background-color', 'rgb(229, 151, 151)');
+            $('h2').text('You lose!')
+            alert('You Lose')
+            unbind();
+            handler();
+        })
+        $('#end').on('click',function(){
+            $('h2').text('YOU WIN!')
+            unbind(); 
+            handler();
+        })
+    })
+    
+}
